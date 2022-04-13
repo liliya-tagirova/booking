21403d91d6e84d0e1c75e17c50dfc7330bc05e03
liliya-tagirova <tor-ver@mail.ru>
2022-04-13 21:36:26 +0300

    Added model User and form for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resource :session, only: %i[new create destroy]
+  resources :users, only: %i[new create edit update]
+
+
   resources :hotels do 
     resources :rooms
     member do
