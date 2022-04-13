f763f3120d1228732969309d75fbc6038f38347a
liliya-tagirova <tor-ver@mail.ru>
2022-04-13 11:54:28 +0300

    Add model Hotel

---
+++ db/migrate/20220413084357_create_hotels.rb
@@ -0,0 +1,16 @@
+class CreateHotels < ActiveRecord::Migration[6.1]
+  def change
+    create_table :hotels do |t|
+      t.string :title
+      t.integer :stars
+      t.string :address
+      t.float :rating
+      t.text :about_hotel
+      t.text :services
+      t.text :extra
+      t.text :contacts
+
+      t.timestamps
+    end
+  end
+end
