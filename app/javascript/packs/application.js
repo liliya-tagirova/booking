d3420dffea8bf42396fff7961adefcae07784ed2
liliya-tagirova <tor-ver@mail.ru>
2022-04-13 21:43:30 +0300

    Fix: delete popper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import '@popperjs/core'
+// import '@popperjs/core'
 import 'bootstrap/js/dist/dropdown'
 
 Rails.start()
