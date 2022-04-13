31080278f0e292e6fc1e5b2e079bacded6283fd8
liliya-tagirova <tor-ver@mail.ru>
2022-04-13 21:57:28 +0300

    Fix: users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the app, #{current_user.name_or_email}!"
+      flash[:success] = "Welcome to the app, #{current_user.name}!"
       redirect_to hotels_path
     else
       render :new
