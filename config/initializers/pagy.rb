e9fe521111ff7efe42b868430fb423974bf5d888
liliya-tagirova <tor-ver@mail.ru>
2022-05-23 15:06:13 +0300

    pagy.rb

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -14,7 +14,7 @@ require 'pagy/extras/bootstrap'
 # Instance variables
 # See https://ddnexus.github.io/pagy/api/pagy#instance-variables
 # Pagy::VARS[:page]   = 1                                  # default
-# Pagy::VARS[:items]  = 20                                 # default
+Pagy::VARS[:items]  = 20                                 # default
 # Pagy::VARS[:outset] = 0                                  # default
 
 # Other Variables
